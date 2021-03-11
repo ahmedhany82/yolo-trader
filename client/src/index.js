603535a16dfde6d242c53c569b91f37d2c8980d3
Ahmed Abdelsalam <ahmedabdelsalam82.de@gmail.com>
2021-03-11 14:31:49 +0100

    changed axios base url

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.css';
 
+axios.defaults.baseURL = '/';
 
 axios.get('/api/auth/loggedin')
   .then(response => {
